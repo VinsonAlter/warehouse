157729026436439173d6ee5d172513969e119497
Vinson <vinson7768@gmail.com>
2023-01-26 13:19:54 +0700

    Update warehouse

--- production/picking-delivery.php
+++ production/picking-delivery.php
@@ -1,96 +1,49 @@
 <?php
-
     session_start();
-
     // require_once '../koneksi.php';
-
     require_once '../function.php';
-
     // cek apakah user sudah login, apabila belum login arahkan user ke laman login
-
     if(!isset($_SESSION['user_login'])) {
-
       header("location:../login.php");  
-
     }
-
     $nama = $_SESSION['user_login'];
-
 ?>
 
 <!DOCTYPE html>
-
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8" />
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
     <!-- Tell the browser to be responsive to screen width -->
-
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <meta name="author" content="Vinson">
-
     <title>PT. Sardana IndahBerlian Motor</title>
-
     <!-- Favicon icon -->
-
     <link
-
       rel="icon"
-
       type="image/png"
-
       sizes="16x16"
-
       href="assets/images/sardana.png"
     />
-
     <!-- Custom CSS -->
-
     <link
-
       rel="stylesheet"
-
       type="text/css"
-
       href="assets/extra-libs/multicheck/multicheck.css"/>
-
     <link
-    
       href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css"
-
       rel="stylesheet"/>
-
     <link href="css/style.min.css" rel="stylesheet" />
-
     <link href="css/styles.css" rel="stylesheet" />
-
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> -->
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <link href="css/datatables.css" rel="stylesheet"/>
-
+    <!-- Bootstrap Datepicker CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css"/>
     <!-- Loading CSS -->
-
     <link href="css/loading.css" rel="stylesheet"/>
-
     <!-- Google Font -->
-    
     <link rel="preconnect" href="https://fonts.googleapis.com">
-
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-
     <style>
 
       body {
@@ -184,43 +137,14 @@
       data-header-position="absolute"
       data-boxed-layout="full"
     >
-      <!-- ============================================================== -->
-      <!-- Topbar header - style you can find in pages.scss -->
-      <!-- ============================================================== -->
-
       <?php require_once 'header-navbar.php';?>
-
-      <!-- ============================================================== -->
-      <!-- End Topbar header -->
-      <!-- ============================================================== -->
-      <!-- ============================================================== -->
-      <!-- Left Sidebar - style you can find in sidebar.scss  -->
-      <!-- ============================================================== -->
       <aside class="left-sidebar" data-sidebarbg="skin5">
-
-        <!-- Sidebar scroll-->
-
         <div class="scroll-sidebar">
-
-          <!-- Sidebar navigation-->
           <?php require_once 'sidebar.php'; ?>
           
-          <!-- End Sidebar navigation -->
         </div>
-        <!-- End Sidebar scroll-->
       </aside>
-
-      <!-- ============================================================== -->
-      <!-- End Left Sidebar - style you can find in sidebar.scss  -->
-      <!-- ============================================================== -->
-
-      <!-- ============================================================== -->
-      <!-- Page wrapper  -->
-      <!-- ============================================================== -->
       <div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
         <div class="page-breadcrumb">
           <div class="row">
             <div class="col-12 d-flex no-block align-items-center">
@@ -231,18 +155,36 @@
             </div>
           </div>
         </div>
-        <!-- ============================================================== -->
-        <!-- End Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-
-        
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
         <div class="row mt-3">
           <div class="col-12">
             <div class="card">
               <div class="card-body">
+                <div class="row">
+                  <form method="post" action="" role="form">
+                    <div class="col-md-8">
+                      <div class="form-group">
+                        <div class="mb-2">
+                          <div class="d-sm-inline-block d-md-flex mb-3">
+                            <label class="mb-3 mb-md-0 mr-half centered d-md-flex">Filter Tgl Transaksi</label>
+                            <input type="text" class="mr-3 col-md-2 col-4 mydatepicker"
+                              name="awalStatus" value="" autocomplete="off">
+                            <label class="centered d-md-flex">s/d.</label>
+                            <input type="text" class="ml-3 col-md-2 col-4 mydatepicker"
+                              name="akhirStatus" value="" autocomplete="off">
+                          </div>
+                          <div class="d-sm-inline-block d-md-flex">
+                            <label class="mb-3 mb-md-0 mr-half centered d-md-flex">Filter Tgl Terima</label>
+                            <input type="text" class="mr-3 col-md-2 col-4 mydatepicker"
+                              name="awalStatus" value="" autocomplete="off">
+                            <label class="centered d-md-flex">s/d.</label>
+                            <input type="text" class="ml-3 col-md-2 col-4 mydatepicker"
+                              name="akhirStatus" value="" autocomplete="off">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
                 <div class="table-responsive">
                   <table id="table_picker" class="table table-bordered table-condensed display compact">
                     <thead>
@@ -252,10 +194,12 @@
                         <th>Tgl Transaksi</th>
                         <th>Customer</th>
                         <th>Status</th>
+                        <th>Tanggal Terima</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
+                        <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
@@ -300,9 +244,20 @@
 
     <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
 
-    <script>
+     <!-- Datepicker JS -->
+     <script src="assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 
-      
+    <script>
+      $(document).ready(() => {
+        /*datepicker*/
+        $(".mydatepicker").datepicker({
+          format: 'dd-mm-yyyy',
+          /* disable sunday in datepicker */
+          daysOfWeekDisabled: "0",
+          autoclose: true,
+          todayHighlight: true
+        });
+      })
 
     </script>
 
