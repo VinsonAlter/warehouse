a9f8701241adb6304bb68bb7c0a3dd6a17058193
Vinson <vinson7768@gmail.com>
2023-01-31 10:14:07 +0700

    Added params for status checking when inserting data

--- production/json/insertTransaksiTerima.php
+++ production/json/insertTransaksiTerima.php
@@ -1,5 +1,6 @@
 <?php
     require_once '../../function.php';
+    date_default_timezone_set('Asia/Jakarta');
     $res = [];
     try {
         if($_POST['batch'] != '') {
@@ -7,23 +8,30 @@
                 $batch = $_POST['batch'];
                 $picker = $_POST['picker'];
                 $arr = explode(' ; ', $batch);
+                $tglTerima = date_to_str(date('d-m-Y'));
                 foreach($arr as $val){
                     $array = explode(' , ', $val);
                     $noTransaksi = $array[0];
                     $tglTransaksi = date_to_str($array[1]);
                     $customer = $array[2];
-                    $insert = "INSERT INTO [WMS-System].[dbo].[TB_Delivery] 
-                            ([NoTransaksi], [Customer], [TglTransaksi], [Status], [PickerID])
-                            VALUES ('".$noTransaksi."', '".$customer."', '$tglTransaksi', 1, '$picker')";
-                    $stmt = $pdo->prepare($insert, [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL]);
-                    $stmt->execute();
-                    if($stmt->rowCount() > 0){
-                        $res['success'] = 1;
-                        $res['insert'] = $insert;
-                        $res['message'] = 'Transaksi berhasil diupdate!';
+                    $status = $array[3];
+                    if($status == "") {
+                        $insert = "INSERT INTO [WMS-System].[dbo].[TB_Delivery] 
+                            ([NoTransaksi], [Customer], [TglTransaksi], [Status], [PickerID], [TglTerima])
+                            VALUES ('$noTransaksi', '$customer', '$tglTransaksi', 1, '$picker', '$tglTerima')";
+                        $stmt = $pdo->prepare($insert, [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL]);
+                        $stmt->execute();
+                        if($stmt->rowCount() > 0){
+                            $res['success'] = 1;
+                            $res['insert'] = $insert;
+                            $res['message'] = 'Transaksi berhasil diupdate!';
+                        } else {
+                            $res['success'] = 0;
+                            $res['message'] = 'Transaksi gagal diupdate, mohon periksa koneksi anda!';
+                        }
                     } else {
                         $res['success'] = 0;
-                        $res['message'] = 'Transaksi gagal diupdate, mohon periksa koneksi anda!';
+                        $res['message'] = 'Status transaksi yang anda pilih sudah diterima!';
                     }
                 }
             } else {
